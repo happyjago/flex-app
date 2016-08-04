4bc4a602d277b24c859e258e93a2c7dac8a6f3b6
happyjago <1336749157@qq.com>
2016-08-04 18:30:56 +0800

    finish

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,22 +1,48 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+var wrap = require('gulp-wrap');
+
+gulp.task('build',function () {
+	gulp.src('pages/*.html')
+		.pipe(wrap({src:'layout/default.html'}))
+		.pipe(gulp.dest('..'))
+})
+
+gulp.task('browser-sync',['sass','build','cp'], function() {
+    browserSync({
+        server: {
+            baseDir: '..'
+        }
+    });
+});
+
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
 
 gulp.task('sass', function(){
-  gulp.src('styles/main.scss')
-      .pipe(sass())
+  gulp.src('styles/*.scss')
+      .pipe(sass()).on('error', handleError)
       .pipe(prefix())
-      .pipe(gulp.dest('../styles'));
+      .pipe(gulp.dest('../styles'))
+      .pipe(browserSync.reload({stream:true}));;
 });
 
 gulp.task('cp',function () {
-	gulp.src('index.html')
-		.pipe(gulp.dest('..'));
+	gulp.src('images/*')
+		.pipe(gulp.dest('../images'));
+})
+
+gulp.task('rebuild',['build'],function () {
+	browserSync.reload();
 })
 
 gulp.task('watch',function () {
-	gulp.watch(['*.html'],['cp']);
-	gulp.watch(['styles/main.scss'],['sass'])
+	gulp.watch(['*/*.html'],['rebuild']);
+	gulp.watch(['styles/*.scss'],['sass'])
 })
 
-gulp.task('default',['sass','cp','watch']);
+gulp.task('default',['browser-sync','watch']);
